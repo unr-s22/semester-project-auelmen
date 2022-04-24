16f9a1105f413023bc3e0b998666fb3cf4ee4575
aarongarza1 <70527694+aarongarza1@users.noreply.github.com>
2022-04-23 20:00:05 -0700

    Create Finalproject.ino
    Aaron Start File

---
+++ Finalproject/Finalproject.ino
@@ -0,0 +1,113 @@
+#include "DHT.h"
+#include <LiquidCrystal.h>
+#define TERMPERATURE_THRESHOLD 80
+#define WATER_LEVEL_THRESHOLD 100 
+#define DHTPIN 2//Whichever pin is used
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+LiquidCrystal lcd(8, 7, 6, 5, 4, 3); //may change pins, we'll see
+ volatile unsigned char *myUCSR0A = (unsigned char *)0x00C0;
+ volatile unsigned char *myUCSR0B = (unsigned char *)0x00C1;
+ volatile unsigned char *myUCSR0C = (unsigned char *)0x00C2;
+ volatile unsigned int  *myUBRR0  = (unsigned int *) 0x00C4;
+ volatile unsigned char *myUDR0   = (unsigned char *)0x00C6;
+
+
+
+enum state {
+   off = 0,
+   idle = 1,
+   temp = 2,
+   water = 3
+};
+
+enum state stat = off;
+
+void setup() {
+  adc_init();
+  Serial.begin(9600);
+  lcd.begin(16, 2) //sixteen columns, 2 rows
+
+}
+
+void loop() {
+  delay(1000);
+  unsigned int w = adc_read(WATER_LEVEL_PORT);
+  float temperature = tempRead(true);
+  float humid = humidRead();// put your main code here, to run repeatedly:
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(f);
+  Serial.print(F(" Water: "));
+  Serial.print(w);
+  Serial.print('\n');
+
+  // Switch uses enumerated stat variable defined above, starting at off
+  switch(stat) {
+    case off:
+      Serial.println("Disabled State");
+      disabled_state();
+      break;
+    case idle:
+      Serial.println("Idle State");
+      idle_state();
+      break;
+    case water:
+      Serial.println("Error State");
+      error_state();
+      break;
+    case temp:
+      Serial.println("Running State");
+      running_state();
+      break;
+    default:
+      break;
+  }
+}
+
+}
+
+
+
+
+void adc_init(void){
+ 
+//16MHz/128 = 125kHz the ADC reference clock
+ 
+ADCSRA |= ((1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0));
+ 
+ADMUX |= (1<<REFS0);       //Set Voltage reference to Avcc (5v)
+ 
+ADCSRA |= (1<<ADEN);       //Turn on ADC
+ 
+ADCSRA |= (1<<ADSC); 
+} 
+
+
+// This function reads temperature from DHT Sensor
+float tempRead() 
+{
+  float t;
+  t = dht.readTemperature(true);   //Fahrenheit
+  if (isnan(t)) Serial.println(F("Failed to read temperature."));
+  return t;
+}
+
+// This function reads humidity from DHT sensor
+float humidRead()
+{
+  float h = dht.readHumidity();
+  if (isnan(h)) Serial.println(F("Failed to read humidity."));
+  return h;
+}
+//This function displays the info to LCD
+void lcd_th(float t, float h) {
+  lcd.setCursor(0, 0);
+  lcd.print("Temp:  Humidity:");
+  lcd.setCursor(0, 1);
+  lcd.print(t);
+  lcd.setCursor(7, 1);
+  lcd.print(h);
+}
