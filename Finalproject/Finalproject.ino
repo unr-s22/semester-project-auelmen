8586062334c6baae923ffeb27d590287d9f27bd8
aarongarza1 <70527694+aarongarza1@users.noreply.github.com>
2022-04-30 18:54:00 -0700

    Create ISR for reset button and for the Stepper Motor

--- Finalproject/Finalproject.ino
+++ Finalproject/Finalproject.ino
@@ -2,25 +2,23 @@
 #include <Stepper.h>
 #include <LiquidCrystal.h>
 #define TEMPERATURE_THRESHOLD 65
-#define WATER_LEVEL_THRESHOLD 100 
+#define WATER_LEVEL_THRESHOLD 300 
 #define DHTPIN 2 //Whichever pin is used
 #define DHTTYPE DHT11
-#define STEPS 30
+const int stepsPerRevolution = 300;  // change this to fit the number of steps per revolution
+// for your motor
 
-Stepper stepper (STEPS, 3, 5, 4, 6);
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 3, 5, 4, 6);
 DHT dht(DHTPIN, DHTTYPE);
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12); //may change pins, we'll see
-unsigned char WATER_LEVEL_PORT = 0;
-volatile unsigned char* my_ADMUX = (unsigned char*) 0x7C;
-volatile unsigned char* my_ADCSRB = (unsigned char*) 0x7B;
-volatile unsigned char* my_ADCSRA = (unsigned char*) 0x7A;
-volatile unsigned int* my_ADC_DATA = (unsigned int*) 0x78;
+unsigned char WATER_LEVEL_PORT = 1;
 volatile unsigned char *port_c = (unsigned char *) 0x28;
 volatile unsigned char *ddr_c = (unsigned char *) 0x27;
 volatile unsigned char *pin_c = (unsigned char *) 0x26;
-volatile unsigned char* port_b = (unsigned char*) 0x25; 
-volatile unsigned char* ddr_b  = (unsigned char*) 0x24; 
-volatile unsigned char* pin_b  = (unsigned char*) 0x23; 
+volatile unsigned char *port_e = (unsigned char *) 0x2E;
+volatile unsigned char *ddr_e = (unsigned char *) 0x2D;
+volatile unsigned char *pin_e = (unsigned char *) 0x2C;
 int potVal = 0;
 int previous =0;
 
@@ -34,21 +32,25 @@ enum state {
 
 enum state stat = off;
 volatile bool ISRReset = 0;
+volatile bool loopStep;
 
 void setup() {
 //  adc_init();
-  stepper.setSpeed(200);
+  myStepper.setSpeed(60);
   dht.begin();
   Serial.begin(9600);
   lcd.begin(16, 2); //sixteen columns, 2 rows
   *ddr_c = 0xFF;
+  *ddr_e = 0x00;
+  *port_e = 0x00;
   *port_c = 0x00;
-  //EICRA |= (1 << ISC10);
-  //EIMSK |= (1 << INT0);
-  *ddr_b = 0x01;
+  PCICR |= (1 << PCIE0);
+  PCMSK0 |= (1 << PCINT0);
+
   PCICR |= (1 << PCIE2);
   
   PCMSK2 |= (1 << PCINT16);
+  analogRead(WATER_LEVEL_PORT);
 }
 
 void loop() {
@@ -68,12 +70,13 @@ void loop() {
   Serial.print(F(" Water: "));
   Serial.print(w);
   Serial.print('\n');
-  lcd_th(temperature, humid);
-//  int val = read_adc(1);
+  if(loopStep)
+  {
+    loopStep = 0;
+    myStepper.step(2*stepsPerRevolution);
+  }
 
-  // move a number of steps equal to the change in the
-  // sensor reading
-  //stepper.step(val - previous);
+
 
   // remember the previous value of the sensor
  // previous = val;
@@ -101,44 +104,7 @@ void loop() {
       break;
   }
 
-  /* 57 PC4 ( A12 ) Digital pin 33
-58  PC5 ( A13 ) Digital pin 32
-59  PC6 ( A14 ) Digital pin 31
-60  PC7 ( A15 ) Digital pin 30
-*port_c = 0b00010000
-*port_c = 0b00100000
-*port_c = 0b01000000
-*port_c = 0b10000000
-
-*/
 }
-//Template for idle, error, and running states (to be tweaked)
-//idle_state()
-    //while(temperature > threshold && water level > too low) {
-      // GREEN LED is ON 
-      // check temperature 
-      // check water level
-      // print transition times }
-    //if (temperature < threshold && water level > too low) {
-      // switch to running (stat = temp)
-      //} else {
-      // switch to error (stat = water)}
-
-//error_state()
-    // while(reset = not pressed && water < too low){
-      // motor is OFF
-      // RED LED is ON (other LEDS are OFF)
-      // print on LCD ("Error")}
-    //switch to idle (stat = idle)
-  
-//running_state()
-    //while(temperature < threshold && water > too low) {
-      // motor is ON
-      // BLUE LED is ON (other LEDS are OFF)}
-    //if (temperature > threshold && water level > too low) {
-      // switch to idle (stat = idle)
-    //} else {
-      // switch to error (stat = water)}
 
 void disabled_state() { // Or off state
   lcd.clear();
@@ -183,7 +149,7 @@ void error_state() {
   unsigned int w = water_level();
 
   // Wait for water level to increase
-  while (w < WATER_LEVEL_THRESHOLD) {
+  while (w < WATER_LEVEL_THRESHOLD && ISRReset == 0) {
     delay(1000);
     w = water_level();
     lcd.setCursor(0, 1);
@@ -192,9 +158,14 @@ void error_state() {
     lcd.print(w);
 }
   
+  
   // Water level is now okay
   stat = idle;
   lcd.clear();
+  if(ISRReset){
+    stat = off;
+  }
+  
 }
 
 void running_state()
@@ -208,11 +179,10 @@ void running_state()
   if (water_level() < WATER_LEVEL_THRESHOLD) stat = water;
   else if ( f > TEMPERATURE_THRESHOLD && ISRReset == 0) {
     delay(1000);
-    lcd.clear();
     Serial.print("Temp: ");
     Serial.print(f);
     Serial.print('\n');
-    //lcd_th(f, h);
+    lcd_th(f, h);
     return running_state();
   }
   else if (ISRReset == 0){
@@ -239,58 +209,42 @@ ISR(PCINT2_vect) {
   }
   }
 }
+ISR(PCINT0_vect){
+  if(PINB & (1 << PB0)) {
+    loopStep = 1;
+}
+}
 
 
 
 unsigned int water_level() {
   return read_adc(WATER_LEVEL_PORT);
 }
-void adc_init() {
-  *my_ADCSRA |= 0b10000000; // sets bit 7 which is ADEN
-  // clear bit 5 and bit 3 and bits 2:0 to 0 to disable the ADC trigger mode, the ADC interrupt, and to set prescaler mode to slow reading
-  *my_ADCSRA &= 0b11010000;
-  *my_ADCSRB &= 0b11110000;
-  *my_ADMUX &= 0b01111111;
-  // set bit   6 to 1 for AVCC analog reference'
-  *my_ADMUX |= 0b01000000;
-  // clear bit 5 to 0 for right adjust result
-  *my_ADMUX &= 0b11011111;
-  *my_ADMUX &= 0b11011111;
-  // clear bit 5 to 0 for right adjust result
+void adc_init(void){
+ 
+//16MHz/128 = 125kHz the ADC reference clock
+ 
+ADCSRA |= ((1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0));
+ 
+ADMUX |= (1<<REFS0);       //Set Voltage reference to Avcc (5v)
+ 
+ADCSRA |= (1<<ADEN);       //Turn on ADC
+ 
+ADCSRA |= (1<<ADSC);
+while(ADCSRA & (1<<ADSC)); 
+} 
 
-  // clear bit 4-0 to 0 to reset the channel and gain bits
-  *my_ADMUX &= 0b11100000;
-}
-
-unsigned int read_adc(unsigned char adc_channel_num) {
-  *my_ADMUX &= 0b11100000;
-  // clear the channel selection bits (MUX 4:0)
-
-  // clear the channel selection bits (MUX 5) bit 3 in ADCSRB
-  *my_ADCSRB &= 0b11110111; 
+uint16_t read_adc(uint8_t channel)
+{
+ADMUX &= 0xE0;           //Clear bits MUX0-4
+ADMUX |= channel&0x07;   //Defines the new ADC channel to be read by setting bits MUX0-2
+ADCSRB = channel&(1<<3); //Set MUX5
+ADCSRA |= (1<<ADSC);      //Starts a new conversion
+while(ADCSRA & (1<<ADSC));  //Wait until the conversion is done
+return ADCW;
+}  
   
-  // set the channel number
-  if (adc_channel_num > 7) {
-    *my_ADCSRB |= 0b00001000;
-    adc_channel_num -= 8;
-  }
-  
-  // set the channel selection bits, but remove the most significant bit (bit 3)
-  //my_ADMUX &= 0xF7;
 
-
-  // set the channel selection bits
-  *my_ADMUX += adc_channel_num;
-  
-  // set bit 6 of ADCSRA to 1 to start a conversion
-  *my_ADCSRA |= 0b01000000;
-
-  // wait for the conversion to complete
-  while ((*my_ADCSRA & 0x40) != 0);
-  
-  // return the result in the ADC data register
-  return *my_ADC_DATA;
-}
 
 // This function reads temperature from DHT Sensor
 float tempRead() 
